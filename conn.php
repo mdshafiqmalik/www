07737d3b6a42f67259e6f935236f0f2ba188f5cb
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 13:06:51 +0530

    fg

--- conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "../../config/raaz.php";
-  $link = new mysqli($host_name, $user_name, $password, $database);
-  if ($link->connect_error) {
-    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
-  } else {
-    echo '<p>Connection to MySQL server successfully established.</p>';
-  }
-?>
