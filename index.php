241c73e00a5f8916ca227278a3425bc9d0d526a8
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 14:52:05 +0530

    asfdgfh

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="twitter:" content="">
+    <title></title>
+  </head>
+  <body>
+
+  </body>
+</html>
 <?php
 include "src/checkdevice.php";
+<?php
+ $output = shell_exec("git pull fastreed main");
+ echo "<pre>$output</pre>";
+ ?>
 
 ?>
