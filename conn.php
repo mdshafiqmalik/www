6d799d8dc83979112f1ef365c0af3d056b6b5cca
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 13:41:14 +0530

    fghj

--- conn.php
+++ conn.php
@@ -4,11 +4,11 @@ function isConnected()
 {
   $link = new mysqli($host_name, $user_name, $password, $database);
  if ($link->connect_error) {
-   $isConnect = die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+   die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
  } else {
-   $isConnect ='<p>Connection to MySQL server successfully established.</p>';
+   echo '<p>Connection to MySQL server successfully established.</p>';
  }
- return $isConnect;
+
 }
 
 ?>
