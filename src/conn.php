82ce42eaa50ab3693f993e109cd3a7d0247e84eb
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 15:15:56 +0530

    sdgfhdg

--- src/conn.php
+++ src/conn.php
@@ -1,5 +1,5 @@
 <?php
-include "../../../config/secrets.php";
+include "../../config/secrets.php";
   $link = new mysqli($host_name, $user_name, $password, $database);
  if ($link->connect_error) {
    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
