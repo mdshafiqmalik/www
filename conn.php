864bc176f72676b77fd24d71d3168bcc838eeb12
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 13:40:08 +0530

    sfdghj

--- conn.php
+++ conn.php
@@ -1,9 +1,14 @@
 <?php
 include "../../config/secrets.php";
-   $link = new mysqli($host_name, $user_name, $password, $database);
-  if ($link->connect_error) {
-    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
-  } else {
-    echo '<p>Connection to MySQL server successfully established.</p>';
-  }
+function isConnected()
+{
+  $link = new mysqli($host_name, $user_name, $password, $database);
+ if ($link->connect_error) {
+   $isConnect = die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+ } else {
+   $isConnect ='<p>Connection to MySQL server successfully established.</p>';
+ }
+ return $isConnect;
+}
+
 ?>
