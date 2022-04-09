a205200155d7fab874f5ceada924b3dfe497dd21
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 15:14:21 +0530

    shftjg

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="twitter:" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-  </head>
-  <body>
-    <?php
-    include "/src/checkdevice.php";
-     ?>
-  </body>
-</html>
+<?php
+include "/src/checkdevice.php";
+ ?>
