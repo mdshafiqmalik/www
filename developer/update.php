3e5cfd8e3301c38ac132630d0eba01a4e8886405
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 15:02:12 +0530

    gdhfg

--- developer/update.php
+++ developer/update.php
@@ -2,13 +2,13 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="twitter:" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
   </head>
   <body>
     <?php
-    include "src/checkdevice.php";
+     $output = shell_exec("git pull fastreed main");
+     echo "<pre>$output</pre>";
      ?>
   </body>
 </html>
