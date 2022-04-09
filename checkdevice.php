4593868fb810b4c1e46bf273e21017dea25f8db7
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-09 14:24:48 +0530

    asfhgjh

--- checkdevice.php
+++ checkdevice.php
@@ -1,5 +1,5 @@
 <?php
-$isMob = function isMobile() {
+$isMob = function() {
     return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 };
  ?>
