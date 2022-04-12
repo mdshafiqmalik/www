2f10689093886ca1bec0a4e5f13af37bc2612d84
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-12 22:17:28 +0530

    dgjhk

--- config/secrets.php
+++ config/secrets.php
@@ -1,6 +1,6 @@
 <?php
-  $host_name = 'db5007199941.hosting-data.io';
-  $database = 'dbs5934513';
-  $user_name = 'dbu3756952';
-  $password = 'TheFastReedSite@123';
+  $hostName = 'db5007199941.hosting-data.io';
+  $dbName = 'dbs5934513';
+  $userName = 'dbu3756952';
+  $passWord = 'TheFastReedSite@123';
 ?>
