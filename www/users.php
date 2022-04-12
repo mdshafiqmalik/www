e74cdd3c14af95df5df08e920bde0e8b71092ce4
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-12 06:30:27 +0530

    dfhgk

--- www/users.php
+++ www/users.php
@@ -2,8 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="user.css?id=12">
+      <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+      <link rel="stylesheet" href="user.css?id=1.1.1">
     <title></title>
     <style media="screen">
 
