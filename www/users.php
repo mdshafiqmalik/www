5eab505b78c46ecba177738adb2257e3d5935fd4
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-12 17:24:27 +0530

    hgj

--- www/users.php
+++ www/users.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <!---<title> Responsive Registration Form | CodingLab </title>--->
-    <link rel="stylesheet" href="user.css?id=8>
+    <link rel="stylesheet" href="user.css?id=8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>
