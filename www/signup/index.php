3c5bb8d1a46a81dccd32f03e9466d7ea839d1e84
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-13 07:00:03 +0530

    dfgh

--- www/signup/index.php
+++ www/signup/index.php
@@ -4,7 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <!---<title> Responsive Registration Form | CodingLab </title>--->
-    <link rel="stylesheet" href="style.css?id=1.1.4">
+    <link rel="stylesheet" href="style.css?id=1.1.8">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
    </head>
