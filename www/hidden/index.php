bedfd9166ee72952cdde37e7e5974fb363b2a909
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-15 12:38:35 +0530

    sdfh

--- www/hidden/index.php
+++ www/hidden/index.php
@@ -2,7 +2,7 @@
 header('content-type:application/json');
 include '../../config/secrets.php';
 $link = new mysqli("$hostName","$userName","$passWord","$dbName");
-if(@isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']=="https://www.shafiqhub.com/signup")
+if(@isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']=="https://www.shafiqhub.com/hidden")
   {
 if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
  //Request identified as ajax request
