6962f379b75018d0b83a69420c1f32fb898128a3
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-15 12:39:56 +0530

    dgfgh

--- www/hidden/index.php
+++ www/hidden/index.php
@@ -2,8 +2,8 @@
 header('content-type:application/json');
 include '../../config/secrets.php';
 $link = new mysqli("$hostName","$userName","$passWord","$dbName");
-if(@isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']=="https://www.shafiqhub.com/hidden")
-  {
+// if(@isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']=="https://www.shafiqhub.com/hidden")
+//   {
 if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
  //Request identified as ajax request
   if ($link->connect_error) {
@@ -34,8 +34,8 @@ if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
 }else {
   echo "access denied";
 }
-}else {
-  echo "access denied";
-}
+// }else {
+//   echo "access denied";
+// }
 
 ?>
