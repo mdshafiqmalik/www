8ff6c838ac1f8ae7c37e4928083a73987d33ec12
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-15 21:01:58 +0530

    dsgfhgd

--- www/register/index.php
+++ www/register/index.php
@@ -5,7 +5,7 @@
     <link rel="stylesheet" href="style.css?id=1.4.1">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-     <title>fastreed - Create An Account</title>
+     <title>FastReed - Create An Account</title>
    </head>
 <body>
 
