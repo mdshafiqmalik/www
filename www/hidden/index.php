cae553294c4865a0870f34d6702b97649fb54756
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-15 12:37:15 +0530

    dftyju

--- www/hidden/index.php
+++ www/hidden/index.php
@@ -1,34 +1,41 @@
 <?php
-// api key = 67834gsd634gsa763g
-// header('content-type:application/json');
+header('content-type:application/json');
 include '../../config/secrets.php';
 $link = new mysqli("$hostName","$userName","$passWord","$dbName");
-if ($link->connect_error) {
-  die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
-}
+if(@isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER']=="https://www.shafiqhub.com/signup")
+  {
+if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+ //Request identified as ajax request
+  if ($link->connect_error) {
+    die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+  }
 
-  $userDataSql =  "SELECT fastUsername, userEmail FROM member_secrets ";
-    if (mysqli_query($link, $userDataSql)) {
-       $res = mysqli_query($link,$userDataSql);
-       if (mysqli_num_rows($res)>0) {
-         $userArray = array();
-         while ($row = mysqli_fetch_assoc($res)) {
-           $userArray[] = $row;
+    $userDataSql =  "SELECT fastUsername, userEmail FROM member_secrets ";
+      if (mysqli_query($link, $userDataSql)) {
+         $res = mysqli_query($link,$userDataSql);
+         if (mysqli_num_rows($res)>0) {
+           $userArray = array();
+           while ($row = mysqli_fetch_assoc($res)) {
+             $userArray[] = $row;
+           }
+             $DataDecode = json_encode($userArray);
+             echo $DataDecode;
+
+         }else {
+           $noData = array("Result"=>"No Data Found");
+           $noDataDecode = json_encode($noData);
+           echo "$noDataDecode";
          }
-           $DataDecode = json_encode($userArray);
-           // $data[] = json_decode($DataDecode, true);
-           // $jason_merge = json_encode($data);
-           echo $DataDecode;
-
-       }else {
-         $noData = array("Result"=>"No Data Found");
-         $noDataDecode = json_encode($noData);
-         echo "$noDataDecode";
-       }
-    }else {
-      $cantRead = array("Result"=>"Cannot Read or connect");
-      $cantReadDecode = json_encode($cantRead);
-      echo "$cantReadDecode";
-    }
+      }else {
+        $cantRead = array("Result"=>"Cannot Read or connect");
+        $cantReadDecode = json_encode($cantRead);
+        echo "$cantReadDecode";
+      }
+}else {
+  echo "access denied";
+}
+}else {
+  echo "access denied";
+}
 
 ?>
