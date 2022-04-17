82803ca8c403bb8a56aea8bfa68eaaccac6760c9
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-17 18:47:13 +0530

    fgdshk

--- mobile/server/hidden/register.php
+++ mobile/server/hidden/register.php
@@ -3,11 +3,11 @@ include 'config/info_m.php';  // Unique
 header('content-type:application/json');
 include '../../../config/__sec__p.php';
 $link = new mysqli("$hostName","$userName","$passWord","$dbName");
-if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
-$thisHttp = $_SERVER['HTTP_REFERER'];
-$url1 = "http://"."$domain"."/account/register/";
-$url2 = "https://"."$domain"."/account/register/";
-if ($thisHttp == $url1 || $thisHttp == $url2) {
+// if($_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest') {
+// $thisHttp = $_SERVER['HTTP_REFERER'];
+// $url1 = "http://"."$domain"."/account/register/";
+// $url2 = "https://"."$domain"."/account/register/";
+// if ($thisHttp == $url1 || $thisHttp == $url2) {
     if ($link->connect_error) {
       die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
     }
@@ -33,16 +33,16 @@ if ($thisHttp == $url1 || $thisHttp == $url2) {
           echo "$cantReadDecode";
         }
 
-  }else {
-    $cantRead = array("Result"=>"Access Denied");
-    $cantReadDecode = json_encode($cantRead);
-    echo "$cantReadDecode";
-  }
-}else {
-  $cantRead = array("Result"=>"Access Denied");
-  $cantReadDecode = json_encode($cantRead);
-  echo "$cantReadDecode";
-}
+  // }else {
+  //   $cantRead = array("Result"=>"Access Denied");
+  //   $cantReadDecode = json_encode($cantRead);
+  //   echo "$cantReadDecode";
+  // }
+// }else {
+//   $cantRead = array("Result"=>"Access Denied");
+//   $cantReadDecode = json_encode($cantRead);
+//   echo "$cantReadDecode";
+// }
 
 
 
