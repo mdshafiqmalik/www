9755c5a04a0332b5f73522074f8f7d1005f657f2
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-18 14:04:21 +0530

    dhfgkh

--- mobile/error/error401.php
+++ mobile/error/error401.php
@@ -2,9 +2,10 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>401 Error</title>
   </head>
   <body>
-    Sorry Page Not Found
+401 Error
   </body>
 </html>
---
+++ mobile/error/error403.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>403 Error</title>
+  </head>
+  <body>
+    403 Error
+  </body>
+</html>
