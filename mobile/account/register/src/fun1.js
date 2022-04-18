f47198e8cd430f4a911ce234463965c388539af6
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-18 14:33:27 +0530

    gjhklj

--- mobile/account/register/src/fun1.js
+++ mobile/account/register/src/fun1.js
@@ -31,144 +31,143 @@ $(document).ready(function(){
     console.log("Focus Out");
   });
 
-  $("unInput").focusin(function(){
+  $("unInput").focus(function(){
     console.log("Focus In");
   });
 });
 
 
+  $("#nmInput").keyup(isNameTrue);
+  function isNameTrue(){
+    let j;
+    let inputValue = $("#nmInput").val();
+    console.log();
+    if ($("#nmInput").val()) {
+      var words = countWords(inputValue);
+      let numbPresent = hasNumber(inputValue);
+      let hasChars = hasSpecialChars(inputValue);
+      if (!hasChars) {
+        if (!numbPresent) {
+         if (words>=2) {
+           showSuccess("#nmAlert",'( Name Accepted &#10003; )');
+           j = true;
+         }else {
+           showWarning("#nmAlert",'( Too Short e.g. John Doe )');
+           j = false;
+         }
+        }else {
+         showWarning("#nmAlert",'( Number not allowed &#x2716; )');
+         j = false;
+        }
+      }else {
+        showWarning("#nmAlert",'(Not allowed &#x2716; )');
+        j = false;
+      }
+    }else {
+      showWarning("#nmAlert",'(Required )');
+      j = false;
+    }
+    return j;
+  }
 
-//   $("#nmInput").keyup(isNameTrue);
-//   function isNameTrue(){
-//     let j;
-//     let inputValue = $("#nmInput").val();
-//     console.log();
-//     if ($("#nmInput").val()) {
-//       var words = countWords(inputValue);
-//       let numbPresent = hasNumber(inputValue);
-//       let hasChars = hasSpecialChars(inputValue);
-//       if (!hasChars) {
-//         if (!numbPresent) {
-//          if (words>=2) {
-//            showSuccess("#nmAlert",'( Name Accepted &#10003; )');
-//            j = true;
-//          }else {
-//            showWarning("#nmAlert",'( Too Short e.g. John Doe )');
-//            j = false;
-//          }
-//         }else {
-//          showWarning("#nmAlert",'( Number not allowed &#x2716; )');
-//          j = false;
-//         }
-//       }else {
-//         showWarning("#nmAlert",'(Not allowed &#x2716; )');
-//         j = false;
-//       }
-//     }else {
-//       showWarning("#nmAlert",'(Required )');
-//       j = false;
-//     }
-//     return j;
-//   }
-//
-// //  for Username
-//   $("#unInput").keyup(isUsernameTrue);
-//   function isUsernameTrue(){
-//     let inputValue = $("#unInput").val();
-//     let j;
-//     if (inputValue) {
-//       if (validateData(inputValue, "username")) {
-//         if (!hasWhiteSpace(inputValue)) {
-//           showSuccess("#unAlert",`( Username is available &#10003; )`);
-//           j = true;
-//         }else {
-//           showWarning("#unAlert",'( Spaces not allowed )' );
-//           j = false;
-//         }
-//       }else {
-//         showWarning("#unAlert",'( Range 6 - 16 letters )');
-//         j = false;
-//       }
-//     }
-//     else {
-//       showWarning("#unAlert",'( Required )');
-//       j = false;
-//     }
-//     return j;
-//   }
-// // For Email
-//   $("#emInput").keyup(isEmailTrue);
-//   function isEmailTrue(){
-//    let toLow = $("#emInput").val();
-//    let inputValue = toLow.toLowerCase();
-//    let j;
-//    $("#emInput").val(inputValue);
-//    if ($("#emInput").val()) {
-//      if (validateData(inputValue, "email")) {
-//        showSuccess("#emAlert",'( E-mail Accepted &#10003; )');
-//        j = true;
-//      }else {
-//        showWarning("#emAlert",'( Invalid Email )');
-//        j = false;
-//      }
-//    }else {
-//      showWarning("#emAlert",'( Required )');
-//      j = false;
-//    }
-//    return j;
-//  }
-// // For password
-//   $("#psInput").keyup(isPasswordTrue);
-//   function isPasswordTrue(){
-//     isConfirmPasswordTrue();
-//    let inputValue = $("#psInput").val();
-//    let i;
-//    if (inputValue) {
-//      if (!hasWhiteSpace(inputValue)) {
-//        if (inputValue.length>8) {
-//          if(passwordStrengthChecker(inputValue)){
-//            i =  true;
-//          }else {
-//            i = false;
-//          }
-//
-//        }else{
-//          $("#psAlert").removeClass("medium");
-//         showWarning("#psAlert",'( Minimum Length 8 Letters )');
-//         i = false;
-//        }
-//      }else {
-//        $("#psAlert").addClass("medium");
-//       showWarning("#psAlert",'(  Avoid Spaces &#10003; )');
-//       i = true;
-//      }
-//    }else {
-//      $("#psAlert").removeClass("medium");
-//      showWarning("#psAlert",'( Required )');
-//      i = false;
-//
-//    }
-//    return i;
-//  }
-// // For Confirm password
-//   $("#confirmPassInput").keyup(isConfirmPasswordTrue );
-//   function isConfirmPasswordTrue(){
-//     let cPassValue = $("#confirmPassInput").val();
-//     let passValue = $("#psInput").val();
-//     if (passValue) {
-//       if (passValue == cPassValue) {
-//          showSuccess("#confirmPassAlert",'( Password Matched &#10003; )');
-//          i = true;
-//       }else {
-//         showWarning("#confirmPassAlert",'( Password Not Match )');
-//         i = false;
-//       }
-//     }else {
-//       showWarning("#confirmPassAlert",'( Required )');
-//       i = false;
-//     }
-//    return i;
-//   }
+//  for Username
+  $("#unInput").keyup(isUsernameTrue);
+  function isUsernameTrue(){
+    let inputValue = $("#unInput").val();
+    let j;
+    if (inputValue) {
+      if (validateData(inputValue, "username")) {
+        if (!hasWhiteSpace(inputValue)) {
+          showSuccess("#unAlert",`( Username is available &#10003; )`);
+          j = true;
+        }else {
+          showWarning("#unAlert",'( Spaces not allowed )' );
+          j = false;
+        }
+      }else {
+        showWarning("#unAlert",'( Range 6 - 16 letters )');
+        j = false;
+      }
+    }
+    else {
+      showWarning("#unAlert",'( Required )');
+      j = false;
+    }
+    return j;
+  }
+// For Email
+  $("#emInput").keyup(isEmailTrue);
+  function isEmailTrue(){
+   let toLow = $("#emInput").val();
+   let inputValue = toLow.toLowerCase();
+   let j;
+   $("#emInput").val(inputValue);
+   if ($("#emInput").val()) {
+     if (validateData(inputValue, "email")) {
+       showSuccess("#emAlert",'( E-mail Accepted &#10003; )');
+       j = true;
+     }else {
+       showWarning("#emAlert",'( Invalid Email )');
+       j = false;
+     }
+   }else {
+     showWarning("#emAlert",'( Required )');
+     j = false;
+   }
+   return j;
+ }
+// For password
+  $("#psInput").keyup(isPasswordTrue);
+  function isPasswordTrue(){
+    isConfirmPasswordTrue();
+   let inputValue = $("#psInput").val();
+   let i;
+   if (inputValue) {
+     if (!hasWhiteSpace(inputValue)) {
+       if (inputValue.length>8) {
+         if(passwordStrengthChecker(inputValue)){
+           i =  true;
+         }else {
+           i = false;
+         }
+
+       }else{
+         $("#psAlert").removeClass("medium");
+        showWarning("#psAlert",'( Minimum Length 8 Letters )');
+        i = false;
+       }
+     }else {
+       $("#psAlert").addClass("medium");
+      showWarning("#psAlert",'(  Avoid Spaces &#10003; )');
+      i = true;
+     }
+   }else {
+     $("#psAlert").removeClass("medium");
+     showWarning("#psAlert",'( Required )');
+     i = false;
+
+   }
+   return i;
+ }
+// For Confirm password
+  $("#confirmPassInput").keyup(isConfirmPasswordTrue );
+  function isConfirmPasswordTrue(){
+    let cPassValue = $("#confirmPassInput").val();
+    let passValue = $("#psInput").val();
+    if (passValue) {
+      if (passValue == cPassValue) {
+         showSuccess("#confirmPassAlert",'( Password Matched &#10003; )');
+         i = true;
+      }else {
+        showWarning("#confirmPassAlert",'( Password Not Match )');
+        i = false;
+      }
+    }else {
+      showWarning("#confirmPassAlert",'( Required )');
+      i = false;
+    }
+   return i;
+  }
   function atLeastOneRadio() {
       return ($('input[type=radio]:checked').size() > 0);
   }
