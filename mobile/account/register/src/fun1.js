4574c8be1e5921d306a24986b7205816ab75cc48
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-18 14:42:34 +0530

    gdjh

--- mobile/account/register/src/fun1.js
+++ mobile/account/register/src/fun1.js
@@ -30,7 +30,7 @@ function getData(url, callback){
     console.log("Focus Out");
   });
 
-  $("#unInput").focusoin(function(){
+  $("#unInput").focusin(function(){
     console.log("Focus In");
   });
 
