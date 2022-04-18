04095bdf5fda68a7aef55c13ba5ae369322bda1a
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-18 18:21:01 +0530

    dfhdiu

--- mobile/server/hidden/register.php
+++ mobile/server/hidden/register.php
@@ -14,7 +14,7 @@ $link = new mysqli("$hostName","$userName","$passWord","$dbName");
     }
     if (isset($_GET["username"])) {
       $inputValue = $_GET["username"];
-      $userDataSql =  "SELECT * FROM member_details Where membUsername = '".$inputValue."' ";
+      $userDataSql =  "SELECT * FROM member_details Where BINARY membUsername = '".$inputValue."' ";
         if (mysqli_query($link, $userDataSql)) {
           $result = mysqli_query($link, $userDataSql);
           if (mysqli_num_rows($result)) {
@@ -33,7 +33,7 @@ $link = new mysqli("$hostName","$userName","$passWord","$dbName");
         }
     }else if(isset($_GET["email"])) {
         $inputValue = $_GET["email"];
-      $userDataSql =  "SELECT * FROM member_details Where membEmail = '".$inputValue."' ";
+      $userDataSql =  "SELECT * FROM member_details Where BINARY membEmail = '".$inputValue."' ";
         if (mysqli_query($link, $userDataSql)) {
           $result = mysqli_query($link, $userDataSql);
           if (mysqli_num_rows($result)) {
