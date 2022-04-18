373fcfdf782e48205ccead37af3b9171970799c3
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-04-18 15:29:19 +0530

    hgkj

--- mobile/account/register/src/fun1.js
+++ mobile/account/register/src/fun1.js
@@ -30,6 +30,7 @@ function getData(url, callback){
     let inputValue = $("#unInput").val();
     getData(`../../../server/hidden/register.php?username=${inputValue}`, function(data){
       console.log(data);
+      console.log(typeof(data.Result));
       if (data.Result == true) {
         console.log(data);
         showWarning("#unAlert",'( Username Taken )' );
