941e663e35bd27e86205fd8c27702c4dfe4c6e67
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-05-06 21:40:54 +0530

    hgh

--- www/test/components/header.php
+++ www/test/components/header.php
@@ -15,9 +15,9 @@ echo '
   </div>
   <div class="subheader">
     <div><a href="/"><img id="home" class="submenu" src="./'.$svgFolder.'/home_NF.svg" alt=""></a></div>
-    <div><a href="channels.php"><img id="channels" class="submenu" src="./'.$svgFolder.'/users_two.svg" alt=""></a></div>
-    <div><img id="" class="submenu" src="./'.$svgFolder.'/bell_NF.svg" alt=""> </div>
-    <div><img id="" class="submenu" src="./'.$svgFolder.'/user_notFilled.svg" alt=""> </div>
+    <div><a href="channels.php"><img id="channels" class="submenu" src="./'.$svgFolder.'/channel.svg" alt=""></a></div>
+    <div><a href="/"><img id="" class="submenu" src="./'.$svgFolder.'/bell_NF.svg" alt=""></a> </div>
+    <div><a href="/"><img id="" class="submenu" src="./'.$svgFolder.'/user_notFilled.svg" alt=""></a> </div>
   </div>
 </div>
 '; ?>
