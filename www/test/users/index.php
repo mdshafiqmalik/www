c4d83b1cfc07504374727e6499fd078218bd4c94
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-05-07 11:40:26 +0530

    jhnh

---
+++ www/test/users/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <?php include '../components/rand.php' ?>
+    <link rel="stylesheet" href="../assets/style.css?v=<?php echo($randVersion); ?>">
+    <link rel="stylesheet" href="../assets/root.css?v=<?php echo($randVersion); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+  </head>
+  <body>
+<?php include '../components/header.php'; ?>
+  </body>
+</html>
