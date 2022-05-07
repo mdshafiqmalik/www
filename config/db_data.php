320655f7a702ce7789571ac217d6e103d37378e3
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-05-07 15:50:26 +0530

    hgj

--- config/db_data.php
+++ config/db_data.php
@@ -3,7 +3,7 @@ if ($_SERVER["HTTP_HOST"] == "m.shafiqhub.com") {
   $hostName = 'db5007199941.hosting-data.io';
   $dbName = 'dbs5934513';
   $userName = 'dbu3756952';
-  $passWord = 'Fequired@k90';
+  $passWord = 'TheFastReedSite@123';
 
 }elseif ($_SERVER["HTTP_HOST"] == "localhost") {
   $hostName = 'localhost';
