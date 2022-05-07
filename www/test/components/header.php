6a748fc4821c09b41e481c7b865ca4e9825ea971
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-05-07 13:11:44 +0530

    fghg

--- www/test/components/header.php
+++ www/test/components/header.php
@@ -16,8 +16,8 @@ echo '
   <div class="subheader">
     <div><a href="/"><img id="home" class="submenu" src="/'.$svgFolder.'/home_NF.svg" alt=""></a></div>
     <!--<div><a href="channels.php"><img id="subscribe" class="submenu" src="/'.$svgFolder.'/subscribe.svg" alt=""></a></div>-->
-    <div><a href="/"><img id="" class="submenu" src="/'.$svgFolder.'/bell_NF.svg" alt=""></a> </div>
-    <div><a href="users"><img id="" class="submenu" src="/'.$svgFolder.'/user_notFilled.svg" alt=""></a> </div>
+    <div><a href="/notifications"><img id="notification" class="submenu" src="/'.$svgFolder.'/bell_NF.svg" alt=""></a> </div>
+    <div><a href="/users"><img id="user" class="submenu" src="/'.$svgFolder.'/user_notFilled.svg" alt=""></a> </div>
   </div>
 </div>
 '; ?>
--- www/test/notifications/index.php
+++ www/test/notifications/index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <style media="screen">
-    #user{
+    #notification{
       filter: var(--svgblue);
     }
     </style>
