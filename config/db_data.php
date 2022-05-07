628d4235ca2c4b7091c1aca44047578c874e580a
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-05-07 15:47:36 +0530

    ghg

--- config/db_data.php
+++ config/db_data.php
@@ -1,1 +1,23 @@
-<?php echo $_SERVER["HTTP_HOST"]; ?>
+<?php
+if ($_SERVER["HTTP_HOST"] == "m.shafiqhub.com") {
+  $hostName = '
+db5007199941.hosting-data.io';
+  $dbName = 'dbs5934513';
+  $userName = 'dbu3756952';
+  $passWord = 'TheExamCenter@123';
+}elseif ($_SERVER["HTTP_HOST"] == "localhost") {
+  $hostName = 'localhost';
+  $dbName = 'fastreed_db';
+  $userName = 'root';
+  $passWord = '';
+}else {
+}
+
+$link = new mysqli("$hostName","$userName","$passWord","$dbName");
+if ($link->connect_error) {
+
+  die('<p>Failed to connect to MySQL: '. $link->connect_error .'</p>');
+}else {
+  echo "Connected";
+}
+?>
