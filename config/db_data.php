32484e009f1442cf2841a5188df8056f5da4e19c
Mohd Shafiq Malik <mdshafiqmalik98@gmail.com>
2022-05-07 15:49:14 +0530

    ghh

--- config/db_data.php
+++ config/db_data.php
@@ -1,10 +1,10 @@
 <?php
 if ($_SERVER["HTTP_HOST"] == "m.shafiqhub.com") {
-  $hostName = '
-db5007199941.hosting-data.io';
+  $hostName = 'db5007199941.hosting-data.io';
   $dbName = 'dbs5934513';
   $userName = 'dbu3756952';
-  $passWord = 'TheExamCenter@123';
+  $passWord = 'Fequired@k90';
+
 }elseif ($_SERVER["HTTP_HOST"] == "localhost") {
   $hostName = 'localhost';
   $dbName = 'fastreed_db';
